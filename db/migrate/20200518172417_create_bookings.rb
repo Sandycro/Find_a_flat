170cf8abcf6eddcce1513d863efd95ad515de886
Ugochukwu Nwokoro <ugonwokoro@yahoo.com>
2020-05-18 18:28:28 +0100

    model added

---
+++ db/migrate/20200518172417_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookings do |t|
+      t.references :user
+      t.references :flat
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
