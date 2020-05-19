961cc3101606b3fae8e3076071fd5be55cd19c47
sandy smircic <sandy.smircic@hotmail.com>
2020-05-19 13:02:20 +0100

    Adding new create

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,20 @@
 class FlatsController < ApplicationController
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @restaurant.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :user)
+  end
 end
