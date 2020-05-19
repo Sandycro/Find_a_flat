852a087cfe0b7980e3accfc906712217fccb9c2c
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-05-19 12:23:01 +0100

    Created the view flats folder

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,14 @@
 class FlatsController < ApplicationController
+  def edit 
+    @flat = Flat.find(params[:id])
+  end
+
+  def update 
+    @flat = Flat.find(params[:id])
+    if @flat.save 
+      redirect_to @flat_path
+    else 
+      render :edit
+    end
+  end
 end
