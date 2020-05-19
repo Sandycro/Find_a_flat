b65ad9443742ceb086e929d05fcae9fa96d31841
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-05-19 15:33:41 +0100

    I've done the edit/ create actions

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,33 +1,19 @@
 class FlatsController < ApplicationController
-  def new
-    @flat = Flat.new
-  end
 
-  def create
-    @flat = Flat.new(flat_params)
-    if @restaurant.save
-      redirect_to flat_path(@flat)
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def flat_params
-    params.require(:flat).permit(:name, :address, :user)
-  end
-
+  
   def edit 
     @flat = Flat.find(params[:id])
   end
 
   def update 
     @flat = Flat.find(params[:id])
-    if @flat.save 
-      redirect_to @flat_path
-    else 
-      render :edit
-    end
+    @flat.update(flat_params)
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:address, :name, :user)
   end
 end
