bed884b0f5d75c96fb4d61e10728cb2d3a0abe5a
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-05-20 13:34:48 +0100

    I create the new, create and destroy methods in the booking controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :flats , except: :destroy do
     resources :bookings, only: [:create , :new]
   end
+  resources :bookings, only: [ :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
