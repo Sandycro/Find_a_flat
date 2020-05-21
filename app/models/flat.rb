34e197221630009aed62fa2af481543ec2df675b
Julian Grosu <iu.grosu@gmail.com>
2020-05-21 17:54:41 +0100

    Mapbox Geocoding added

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -5,4 +5,6 @@ class Flat < ApplicationRecord
   validates :address, presence: true
   has_one_attached :photo
   validates :photo, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_103110) do
+ActiveRecord::Schema.define(version: 2020_05_21_115528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_05_20_103110) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
